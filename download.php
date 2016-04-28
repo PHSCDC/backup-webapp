11ca919760d73ef69d1a97ac17d77364725aa9bf
Jimmy K <jimmyk3.14159@gmail.com>
2016-04-27 20:52:51 -0500

    Proper sanitization

--- download.php
+++ download.php
@@ -1,9 +1,10 @@
 <?php
+require 'clean.php';
 header('Content-type: application/x-bzip2');
 
 $file = $_GET['file'];
 
-header('Content-disposition: attachment; filename="'.$file.'"');
+header('Content-disposition: attachment; filename="'.clean($file).'"');
 
-readfile('/backup/'.$file);
+readfile('/backup/'.clean($file));
 ?>
--- list.php
+++ list.php
@@ -1,18 +1,20 @@
 <html>
 	<head>
 <?php
+	require 'clean.php';
 	$server = $_GET['server'];
+	
 ?>
-		<title>Backups of <?php echo($server) ?></title>
+		<title>Backups of <?php echo(clean($server)) ?></title>
 	</head>
 	<body>
 		<h1>Backup server status</h1>
 		<p><b>Disk usage: </b><?php echo(shell_exec("df -h | grep /$ | grep -o '[0-9]*%'")); ?></p>
 
-		<h2>Backups of <?php echo($server)?></h1>
-		<b>Total: </b><?php echo(shell_exec("ls /backup | grep $server | wc -l")); ?>
+		<h2>Backups of <?php echo(clean($server))?></h1>
+		<b>Total: </b><?php echo(shell_exec("ls /backup | grep ".clean($server)." | wc -l")); ?>
 		<ul>
-			<?php echo(shell_exec("ls /backup | grep $server | sed 's/\\(.*\\)/<li><a href=\"download.php?file=\\1\">\\1<\\/a><\\/li>/'")); ?>
+			<?php echo(shell_exec("ls /backup | grep ".clean($server)." | sed 's/\\(.*\\)/<li><a href=\"download.php?file=\\1\">\\1<\\/a><\\/li>/'")); ?>
 		</ul>	
 		<a href="/">Back</a>
 	</body>
